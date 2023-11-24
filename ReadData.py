bdb2b0d31ba99225c140f9cda86447c0b69d968c
AdustH <adusth0921@gmail.com>
2023-11-24 17:06:03 +0800

    将速度作为数组传递

--- ReadData.py
+++ ReadData.py
@@ -5,11 +5,11 @@ import numpy
 #使用pyvista库从目标文件夹中读取fluent保存的.cas和.dat文件
 #因为pyvista的Unstructured Grid类型无法在simulink中作为参数传递
 #所以这里将dataset设定为本程序下的全局变量，
-dataset = pyvista.read("../data/gan20.cas")
+dataset = pyvista.read("../data/KC17.cas")
 # 上行保读取的文件类型是分块网格，需要将其合并成非结构网格
 dataset = dataset.combine()
 
-def get_pressure( x, y, z):
+def get_pressure(x, y, z):
  # 创建PolyData对象，包含指定坐标的单个点
         poly_data = pyvista.PolyData(numpy.array([[x, y, z]]))
         # 使用probe方法获取压力值
@@ -40,3 +40,8 @@ def get_density(x, y, z):
         # 创建PolyData对象，包含指定坐标的单个点
         poly_data = pyvista.PolyData(numpy.array([[x, y, z]]));
         return poly_data.sample(dataset)["DENSITY"][0]
+
+def get_velocity(x, y, z):
+        poly_data = pyvista.PolyData(numpy.array([[x, y, z]]));
+        velocity = [poly_data.sample(dataset)["X_VELOCITY"][0], poly_data.sample(dataset)["Y_VELOCITY"][0], poly_data.sample(dataset)["Z_VELOCITY"][0]];
+        return velocity
